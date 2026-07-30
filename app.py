7139a5396a1f78ffaf16c3ba80e6fd69ebb28514
Samir Khan <sksamirkhan83@gmail.com>
2026-07-30 10:55:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,6 +42,14 @@ elif all(all_API):
 else:
     st.info("PASS ALL API-KEYS")
 
+
+# MULTISELECT OPTION
+options = ["Delhi","Mumbai","Pune","Banglore","Gurugram/Gurgaon"]
+location = st.sidebar.multiselect("Select Location",options = options)
+
+profile_op = ["Data Analysts","AI Engineer","Gen AI Developer","Full-Stack-Dev","Data Scientist"]
+profile = st.sidebar.multiselect("Select Job Profile",options = profile_op)
+
 #===========MODEL CREATION==============
 model = ChatGoogleGenerativeAI(
     model = 'gemini-3.5-flash-lite',
